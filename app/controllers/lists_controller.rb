8cdef22577a9d998c6f173c27e53d28ab91f8332
minoriakita <minori.akita@gmail.com>
2022-04-06 10:52:51 +0000

    [Update] 3-４ 詳細画面作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,12 +3,29 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    list = List.new(list_params)
+    
+    list.save
+    
+    redirect_to list_path(list.id)
+  end
+
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
